367c8b19f8ea81fbfdb9c91b7227ab7a2b1d73c7
kedarjoshi <kedar2386@gmail.com>
2019-12-14 12:35:10 -0600

    load `chromedriver` from classpath

--- web/src/test/java/com/example/demo/ui/config/SeleniumConfig.java
+++ web/src/test/java/com/example/demo/ui/config/SeleniumConfig.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.net.URL;
 
 @SpringBootTest
 public class SeleniumConfig {
@@ -15,10 +19,14 @@ public class SeleniumConfig {
 
   @BeforeEach
   public void setUp() {
-    System.setProperty("webdriver.chrome.driver", "chromedriver");
     ChromeOptions options = new ChromeOptions();
     options.addArguments("headless");
-    driver = new ChromeDriver(options);
+    URL url = this.getClass().getClassLoader().getResource("chromedriver");
+    File file = new File(url.getFile()); // Strangely, URL.getFile does not return a File
+    ChromeDriverService.Builder bldr = (new ChromeDriverService.Builder())
+            .usingDriverExecutable(file)
+            .usingAnyFreePort();
+     driver = new ChromeDriver(bldr.build(), options);
   }
 
   protected WebElement getElementById(String id) {
